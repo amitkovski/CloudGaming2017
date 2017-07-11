6c638fd3f198170663b7695a58fb99eca0d16ca0
MoritzMeyer <privat.moritzmeyer@googlemail.com>
2017-07-11 12:49:07 +0200

    port anpassen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var port = process.env.Port || 3000;
+var port = 80; //process.env.Port || 3000;
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var LdapStrategy = require('passport-ldapauth');
