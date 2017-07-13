16b56964497e0d96bdf8b481f54970229a580d5a
MoritzMeyer <privat.moritzmeyer@googlemail.com>
2017-07-13 10:10:17 +0200

    changed https => http

--- public/js/login.js
+++ public/js/login.js
@@ -22,7 +22,7 @@ function onLoginError(user, error) {
 
 function login(username, password, cbSuccess, cbError) {
  $.ajax({
-    url: "https://gamingservice.azurewebsites.net/login",
+    url: "http://gamingservice.azurewebsites.net/login",
     type: 'POST',
     dataType: 'json',
     data: {'username':username, 'password':password}, 
